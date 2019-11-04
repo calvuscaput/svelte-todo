fac12f10bc497c3b69777ed0840e2c567fc1c2b9
CalvusCaput <calvuscaput@yandex.ru>
2019-11-04 13:16:17 +0300

    TodoItem. template готов

---
+++ src/TodoItem.svelte
@@ -0,0 +1,27 @@
+<div class="todo-item">
+  <div
+    class="todo-item-left"
+    transition:fly="{{
+      y: 20, 
+      duration: 300
+    }}"
+  >
+    <input
+      type="checkbox"
+      bind:checked={completed}
+      on:change={toggleComplete}
+    >
+    <div
+      class="todo-item--label"
+      class:completed={completed}
+    >
+      {title}
+    </div>
+  </div>
+  <div
+    class="remove-item"
+    on:click={deleteTodo}
+  >
+    x
+  </div>
+</div>
